72cd223d3d94b00f6d9679df562b9bf0be3f20e1
Hasan <hassan.m.h.m.a123@gmail.com>
2020-09-16 13:34:26 -0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,112 @@
+$(window).on('load', function () {
+
+  //preloader
+  $('.pre-loader').fadeOut("500", function () {
+    $('html').removeClass("overflow");
+    $(this).remove();
+  });
+
+  //nava toggle
+  $('#nava-icon').click(function (e) {
+    $('#nava').toggleClass('nava-active');
+    $('html').toggleClass('overflow');
+  });
+
+  $('#nava').click(function (e) {
+    if (e.target.id == 'nava' || e.target.id == 'close-nava' || e.target.parentNode.id == "close-nava") {
+      $(this).removeClass('nava-active');
+      $('html').removeClass('overflow');
+    }
+  });
+
+  //search toggle
+  $('#search-btn').click(function (e) {
+    $('#search-form').toggleClass('search-form-active');
+    $('html').toggleClass('overflow');
+  });
+
+  $('#search-form').click(function (e) {
+    if (e.target.id == 'search-form' || e.target.id == 'close-search' || e.target.parentNode.id == "close-search") {
+      $(this).removeClass('search-form-active');
+      $('html').removeClass('overflow');
+    }
+  });
+
+  $(".slide").on("click", function (e) {
+    if (e.target.classList.contains('drop')) {
+      console.log(e.target);
+      $(this).toggleClass("slide-active");
+      $(this).children("ul").slideToggle();
+    }
+  });
+
+
+
+  var mySwiper = new Swiper('header .swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    updateOnWindowResize: true,
+    autoplay: {
+      delay: 5000,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  var mySwiper = new Swiper('#clients .swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    updateOnWindowResize: true,
+    slidesPerView: 6,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 5000,
+    },
+    breakpoints: {
+      992: {
+        slidesPerView: 6,
+      },
+      576: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 250px
+      0: {
+        slidesPerView: 2,
+      }
+    },
+
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+  // animate on scroll
+
+  AOS.init({
+    duration: 700,
+    mirror: true,
+    once: true,
+    disable: function () {
+      var maxWidth = 769;
+      return window.innerWidth < maxWidth;
+    },
+    easing: 'ease-in-out'
+  });
+
+
+
+});
